f7c549aea4489a2f4597b2b49f25769787b50949
Michael Caira <mbcaira@gmail.com>
2023-03-15 09:45:05 -0400

    fix encoding in tracker socket, track spherical point

--- sim/Source/RP04/Private/RoboticArm.h
+++ sim/Source/RP04/Private/RoboticArm.h
@@ -26,23 +26,12 @@ public:
 	// Define robotic arm constraints and measurement
 	const TArray<FString> JOINT_AXES = {"Y", "Y", "Y", "Y", "Y", "Y"};
 
-	struct JointAngles
-	{
-		float Q1;
-		float Q2;
-		float Q3;
-		float Q4;
-		float Q5;
-		float Q6;
-	};
-
 	// void MoveEndEffector(FVector DesiredPosition);
 
 private:
 	FString LocalIP;
 	int32 LocalPort;
 	FSocket* ListenerSocket;
-
 	TArray<uint8> ReceivedData;
 
 	UStaticMeshComponent* Joints[6];
@@ -52,4 +41,6 @@ private:
 	FVector ParseCoordinates(FString stream);
 
 	void SetJointConfig(float Q1, float Q2, float Q3, float Q4, float Q5, float Q6);
+
+	void CalculateJointConfig(FVector coords);
 };
